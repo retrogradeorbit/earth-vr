bf0ec64ea23cb7363996a46eb2226362c029f8b2
Crispin Wellington <retrogradeorbit@gmail.com>
2016-03-26 22:32:45 +0800

    try unsucessfully at an oculus camera approach

--- src/earth_vr/canvas.cljs
+++ src/earth_vr/canvas.cljs
@@ -34,7 +34,8 @@
   canvas"
 
   []
-  (let [renderer (js/THREE.WebGLRenderer.)
+  (let [renderer  (js/THREE.WebGLRenderer.)
+
         actual-canvas (.-domElement renderer)]
     (.setSize renderer
               (.-innerWidth js/window)
@@ -52,21 +53,106 @@
 
 (defn init
   []
-  (let [{:keys [renderer] :as world} (make)
+  (let [{:keys [renderer canvas] :as world} (make)
         scene (js/THREE.Scene.)
         camera (let [cam (js/THREE.PerspectiveCamera.
                           75 (/ (.-innerWidth js/window) (.-innerHeight js/window)) 1 10000)]
-                 (set! (.-position.z cam) 1000)
-                 cam)]
+                 (set! (.-position.z cam) 50)
+                 (set! (.-position.y cam) -20)
+                 (set! (.-rotation.x cam) 0.4)
+                 cam)
+
+        ;controls (js/THREE.OrbitControls. camera (.-domElement renderer))
+
+        separation 3
+        focal-length 15
+
+        camera-l (js/THREE.PerspectiveCamera.)
+        camera-r (js/THREE.PerspectiveCamera.)
+
+        ]
+
     (.render renderer scene camera)
 
     (let [
           render-fn #(.render renderer scene camera)
+
+          render-fn-2
+          #(do
+             (.updateMatrixWorld scene)
+             (.updateMatrixWorld camera)
+
+             (let [pos (js/THREE.Vector3.)
+                   quat (js/THREE.Quaternion.)
+                   scale (js/THREE.Vector3.)
+                   ]
+               (.decompose (.-matrixWorld camera) pos quat scale)
+
+               (let [
+                     width (.-innerWidth js/window)
+                     height (.-innerHeight js/window)
+
+                     fov (->
+                          (.-fov camera)
+                          js/THREE.Math.degToRad
+                          (* 0.5)
+                          Math/tan
+                          Math/atan
+                          (* 2)
+                          js/THREE.Math.radToDeg)
+                     ndfl (/ (.-near camera) focal-length)
+                     half-focal-height (-> fov
+                                           js/THREE.Math.degToRad
+                                           (* 0.5)
+                                           Math/tan
+                                           (* focal-length))
+                     half-focal-width (* half-focal-height (.-aspect camera) 0.5)
+                     top (* half-focal-height ndfl)
+                     bottom (- top)
+                     inner-factor (/ (+ half-focal-width (/ separation 2))
+                                     (* 2.0 half-focal-width))
+                     outer-factor (- 1.0 inner-factor)
+
+                     outer (* half-focal-width 2.0 ndfl outer-factor)
+                     inner (* half-focal-width 2.0 ndfl inner-factor)]
+
+                 ;; left
+                 (-> camera-l
+                     .-projectionMatrix
+                     (.makeFrustum (- outer) inner bottom top
+                                   (.-near camera) (.-far camera)))
+
+                 (-> camera-l .-position (.copy pos))
+                 (-> camera-l .-quaternion (.copy quat))
+                 (-> camera-l (.translateX (- (/ separation 2.0))))
+
+                 ;; right
+                 (-> camera-r
+                     .-projectionMatrix
+                     (.makeFrustum (- inner) outer bottom top
+                                   (.-near camera) (.-far camera)))
+
+                 (-> camera-r .-position (.copy pos))
+                 (-> camera-r .-quaternion (.copy quat))
+                 (-> camera-r (.translateX (- (/ separation 2.0))))
+
+                 (.setViewport renderer 0 0 (* 2 width) height)
+                 (.clear renderer)
+
+                 (js/console.log "camera-l" (str camera-l))
+                 (.setViewport renderer 0 0 width height)
+                 (.render renderer scene camera-l)
+
+                 (js/console.log "camera-r" (str camera-r))
+                 (.setViewport renderer width 0 width height)
+                 (.render renderer scene camera-r))))
+
           resize-fn (fn [width height]
                       (.setSize renderer width height)
                       (set! (.-aspect camera) (/ width height))
                       (.updateProjectionMatrix camera)
                       )
+
           expand-fn #(resize-fn (.-innerWidth js/window)
                                 (.-innerHeight js/window))
 
