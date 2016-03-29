d0572c3df3fe61cac4593a2186f1302316fabdb0
Crispin Wellington <retrogradeorbit@gmail.com>
2016-03-29 21:35:07 +0800

    start oculus shaders

--- src/earth_vr/renderer.cljs
+++ src/earth_vr/renderer.cljs
@@ -1,6 +1,7 @@
 (ns earth-vr.renderer
   (:require [earth-vr.canvas :as c]))
 
+;; plain stereo view renderer
 (defmethod c/make-render-fn :stereo [options renderer scene camera]
   #(do
     (.updateMatrixWorld scene)
@@ -77,7 +78,52 @@
         (.render renderer scene camera-l)
 
         (.setViewport renderer half-width 0 half-width height)
-        (.render renderer scene camera-r))))
-  )
+        (.render renderer scene camera-r)))))
 
-(defn foo [])
+;;
+;; port of javascript oculus rift renderer
+;;
+
+(def vertex-shader
+"
+varying vec2 vUv;
+
+void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
+")
+
+(def fragment-shader
+"
+uniform vec2 scale;
+uniform vec2 scaleIn;
+uniform vec2 lensCenter;
+uniform vec4 hmdWarpParam;
+uniform vec4 chromAbParam;
+uniform sampler2D texid;
+varying vec2 vUv;
+void main()
+{
+  vec2 uv = (vUv*2.0)-1.0;
+  vec2 theta = (uv-lensCenter)*scaleIn;
+  float rSq = theta.x*theta.x + theta.y*theta.y;
+  vec2 rvector = theta*(hmdWarpParam.x + hmdWarpParam.y*rSq + hmdWarpParam.z*rSq*rSq + hmdWarpParam.w*rSq*rSq*rSq);
+  vec2 rBlue = rvector * (chromAbParam.z + chromAbParam.w * rSq);
+  vec2 tcBlue = (lensCenter + scale * rBlue);
+  tcBlue = (tcBlue+1.0)/2.0;
+  if (any(bvec2(clamp(tcBlue, vec2(0.0,0.0), vec2(1.0,1.0))-tcBlue))) {
+    gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+    return;}
+  vec2 tcGreen = lensCenter + scale * rvector;
+  tcGreen = (tcGreen+1.0)/2.0;
+  vec2 rRed = rvector * (chromAbParam.x + chromAbParam.y * rSq);
+  vec2 tcRed = lensCenter + scale * rRed;
+  tcRed = (tcRed+1.0)/2.0;
+  gl_FragColor = vec4(texture2D(texid, tcRed).r, texture2D(texid, tcGreen).g, texture2D(texid, tcBlue).b, 1);
+}
+")
+
+(defmethod c/make-render-fn :oculus [options renderer scene camera]
+
+)
